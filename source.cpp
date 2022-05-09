05ddbe3633ec8d40a549e1352cfb63401cf3ad39
CzDelta <kido137@naver.com>
2022-05-10 08:55:56 +0900

    갱신

--- source.cpp
+++ source.cpp
@@ -1,71 +1,48 @@
 #include <iostream>
-
-#define MAX 52
-
-#define MAX10(X) (X > 10) ? 10 : X
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int Deck[MAX] = { 0, };
+	char Buffer[1024] = { 0, };
 
-	for (int i = 0; i < 52; ++i)
+	
+		cin >> Buffer;
+
+	int Size = 0;
+
+	for (int i = 0; i < 1024; ++i)
 	{
-		Deck[i] = (i % 13) + 1;
-	}
-
-	srand(static_cast<unsigned int>(time(NULL)));
-
-
-	int ShuffleCount = (rand() % 20) + 2;
-	for (int i = 0; i < MAX * ShuffleCount; ++i)
-	{
-		int FirstNumber = rand() % MAX;
-		int SecondNumber = rand() % MAX;
-		int Temp = Deck[FirstNumber];
-		Deck[FirstNumber] = Deck[SecondNumber];
-		Deck[SecondNumber] = Temp;
-	}
-
-	int Player[3] = { 0, };
-	int AI[3] = { 0, };
-
-	for (int i = 0; i < 3; ++i)
-	{
-		Player[i] = Deck[i * 2];
-		AI[i] = Deck[(i * 2) + 1];
-	}
-
-	int PlayerSum = 0;
-	int AISum = 0;
-
-	for (int i = 0; i < 3; ++i)
-	{
-		PlayerSum += MAX10(Player[i]);
-		AISum += MAX10(AI[i]);
-	}
-	cout << "Player : " << PlayerSum << endl;
-	cout << "AI : " << AISum << endl;
-
-	if (AISum >= 22)
-	{
-		cout << "Player Win" << endl;
-	}
-	else if (AISum <= 21 && PlayerSum <= 21)
-	{
-		if (PlayerSum >= AISum)
+		if (Buffer[i] == '\0')
 		{
-			cout << "Player Win" << endl;
-		}
-		else
-		{
-			cout << "AI Win" << endl;
+			Size = i + 1;
 		}
 	}
-	else
+
+	char* Array = new char[Size];
+
+	for (int i = 0; i < Size; ++i)
 	{
-		cout << "AI Win" << endl;
+		Array[i] = Buffer[i];
 	}
+
+	cout << Array << endl;
+
+	//for (int i = 0; i < Size; ++i)
+	//{
+	//	Array[i] = i + 1;
+	//}
+
+	//for (int i = 0; i < Size; ++i)
+	//{
+	//	cout << Array[i] << ", ";
+	//}
+
+	delete[] Array;
+
 	return 0;
+
+	
+
 }
